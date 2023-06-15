e7227813453a4b5eea937957dd47c217a29762b1
Kimonas Tsiliras <kimonas.tsiliras@gmail.com>
2023-06-15 13:48:23 +0300

    Added custom percentages per skill

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -20,14 +20,17 @@ function reveal(){
 window.addEventListener('scroll',revealProgress);
     function revealProgress(){
         var progressBars = document.querySelectorAll('.progress-bar');
+        var percentages = ["75%","75%","68%","64%","64%","64%","62%","60%","60%","55%","55%","55%"]
+
         for(var i=0; i<progressBars.length; i++){
             var windowHeight = window.innerHeight;
             var revealtop = progressBars[i].getBoundingClientRect().top;
             if(revealtop < windowHeight){
                 progressBars[i].classList.add('active');
-    
+                progressBars[i].style.setProperty("--width",percentages[i])
             }else{
                 progressBars[i].classList.remove('active');
+                progressBars[i].style.setProperty("--width","0%")
             }
         }
     }
