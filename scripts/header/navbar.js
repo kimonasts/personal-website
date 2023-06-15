cf0a92248ad43c2459ce725cfbe69e8b45a47079
Kimonas Tsiliras <kimonas.tsiliras@gmail.com>
2023-06-15 18:59:48 +0300

    Fixed navbar responsiveness

--- scripts/header/navbar.js
+++ scripts/header/navbar.js
@@ -6,3 +6,44 @@ document.addEventListener('scroll', ()=>{
         navbar.classList.remove('scrolled')
     }
 })
+
+document.querySelector('.navbar-toggler').addEventListener("click",()=>{
+
+    navbar = document.querySelector('.navbar');
+    navLinks = document.querySelectorAll('.nav-link');
+    togglerIcon = document.querySelector('.navbar-toggler-icon');
+    ariaExpanded = document.getElementById("toggler").getAttribute('aria-expanded');
+
+    if(ariaExpanded==="true")
+    {
+        togglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
+
+        navbar.style.transition = "background-color 0.7s ease-in-out";
+        navbar.style.backgroundColor = "rgba(250, 250, 250, 0.89)";
+        navbar.style.opacity="0.98";
+        navbar.style.backdropFilter = "saturate(180%) blur(20px)";
+
+        for(var i=0; i<navLinks.length; i++){
+            navLinks[i].style.color = "black";
+        }
+    }
+    else{
+        if(window.scrollY >= 477){
+            togglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
+
+        } else{
+            togglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
+
+        }
+
+        navbar.style.transition = "";
+        navbar.style.backgroundColor = "";
+        navbar.style.opacity="";
+        navbar.style.backdropFilter = "";
+
+        for(var i=0; i<navLinks.length; i++){
+            navLinks[i].style.color = "white";
+        }
+    }
+    
+})
--- scripts/reveal.js
+++ scripts/reveal.js
@@ -2,12 +2,18 @@ window.addEventListener('scroll',reveal);
 
 function reveal(){
     var reveals = document.querySelectorAll('.reveal');
+    var navbarTogglerIcon = document.querySelector('.navbar-toggler-icon');
 
+    if (window.scrollY >= 477){
+        navbarTogglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
+    }
+    else{
+        navbarTogglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
+    }
     for(var i=0; i<reveals.length; i++){
         var windowHeight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
         var revealpoint=50;
-
         if(revealtop < windowHeight - revealpoint){
             reveals[i].classList.add('active');
 
