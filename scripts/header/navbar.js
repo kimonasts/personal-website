04b523e5dc3722946b1ff58b1afb926a8db66b9d
Kimonas Tsiliras <kimonas.tsiliras@gmail.com>
2023-09-08 20:40:01 +0300

    Changes and additions to components

--- scripts/header/navbar.js
+++ scripts/header/navbar.js
@@ -9,7 +9,7 @@ document.addEventListener('scroll', ()=>{
     var navbarTogglerIcon = document.querySelector('.navbar-toggler-icon');
     var ariaExpanded = document.getElementById("toggler").getAttribute('aria-expanded');
 
-    if (window.scrollY >= 477 || ariaExpanded === "true"){
+    if (window.scrollY >= 577 || ariaExpanded === "true"){
         navbarTogglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
     }
     else{
@@ -38,7 +38,7 @@ document.querySelector('.navbar-toggler').addEventListener("click",()=>{
         }
     }
     else{
-        if(window.scrollY >= 477){
+        if(window.scrollY >= 577){
             togglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
         } else{
             togglerIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
