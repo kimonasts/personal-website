b6ab5b6819ea4ea49794390031fcf7fbdc054bd8
Kimonas Tsiliras <kimonas.tsiliras@gmail.com>
2023-09-13 23:14:06 +0300

    Commit

--- scripts/submit-form.js
+++ scripts/submit-form.js
@@ -1,10 +1,19 @@
 document.getElementById("contact-form").addEventListener("submit",function(event){
     event.preventDefault();
 
+    // Select the form element by ID
+    const form = document.getElementById("contact-form");
+
+    // Create a new FormData object from the form
+    const formData = new FormData(form);
+
     // Reset the form fields
     this.reset();
 
-    // Redirect to the specified target page
-    const targetPage = this.getAttribute("action");
-    window.location.href = targetPage;
+    fetch("https://formspree.io/f/mnqkvqdy", {
+      method: "POST",
+      body: formData
+    }).then(()=>{
+      window.location.href = "https://formspree.io/f/mnqkvqdy";
+    })
 })
