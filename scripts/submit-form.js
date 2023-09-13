87f2eb37accef828c63e7fa5e274dbf7d759a16a
Kimonas Tsiliras <kimonas.tsiliras@gmail.com>
2023-09-13 21:26:02 +0300

    Changes in form submissions

--- scripts/submit-form.js
+++ scripts/submit-form.js
@@ -1,4 +1,4 @@
-document.getElementById("contact-form").addEventListener("submit", function (event) {
+document.getElementById("submit-button").addEventListener("click", function (event) {
     // Prevent the default form submission behavior
     event.preventDefault();
     
